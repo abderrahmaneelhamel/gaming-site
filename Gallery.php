3c130c0c2a8c9151b70dad13152b7d87cc299e0e
abderrahmaneelhamel <abdoelhamel359@gmail.com>
2022-11-23 10:04:42 +0100

    new change

--- Gallery.php
+++ Gallery.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Playtech</title>
     <link rel="stylesheet" href="Style.css">
    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
    <link rel="stylesheet" href="css/bootstrap.css">
